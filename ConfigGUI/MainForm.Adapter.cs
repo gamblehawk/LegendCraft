1815285f36b279762f96ffe59f47218a2c002ba3
LeChosenOne <jonahw@optonline.net>
2012-11-11 18:55:54 -0500

    removed haspaid, isn't mojang awesome?

--- ConfigGUI/MainForm.Adapter.cs
+++ ConfigGUI/MainForm.Adapter.cs
@@ -282,8 +282,6 @@ namespace fCraft.ConfigGUI {
                 cPatrolledRank.SelectedIndex = RankManager.GetIndex( RankManager.PatrolledRank );
             }
 
-            xPaidPlayersOnly.Checked = ConfigKey.PaidPlayersOnly.Enabled();
-
 
             xBlockDBEnabled.Checked = ConfigKey.BlockDBEnabled.Enabled();
             xBlockDBAutoEnable.Checked = ConfigKey.BlockDBAutoEnable.Enabled();
@@ -537,7 +535,6 @@ namespace fCraft.ConfigGUI {
             } else {
                 ConfigKey.PatrolledRank.TrySetValue( RankManager.PatrolledRank.FullName );
             }
-            ConfigKey.PaidPlayersOnly.TrySetValue( xPaidPlayersOnly.Checked );
 
             ConfigKey.BlockDBEnabled.TrySetValue( xBlockDBEnabled.Checked );
             ConfigKey.BlockDBAutoEnable.TrySetValue( xBlockDBAutoEnable.Checked );
