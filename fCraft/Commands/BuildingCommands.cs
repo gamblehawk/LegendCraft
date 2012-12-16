5812f7acb443afadbe8ef9a6836575f54712a70a
LeChosenOne <jonahw@optonline.net>
2012-12-16 14:17:40 -0500

    Eeyle added to LC Team

--- fCraft/Commands/BuildingCommands.cs
+++ fCraft/Commands/BuildingCommands.cs
@@ -103,7 +103,7 @@ namespace fCraft {
             //CommandManager.RegisterCommand(CdBanX2);
         }
         #region LegendCraft
-        /* Copyright (c) <2012> <LeChosenOne, DingusBungus>
+        /* Copyright (c) <2012> <LeChosenOne, DingusBungus, Eeyle>
    Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
