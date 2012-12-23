b1770d0f9aea380eaed30e19ff2ad83dcad97abc
Eeyle <imabearnose@gmail.com>
2012-12-23 11:15:48 -0800

    Added /AbortAll

--- fCraft/Commands/BuildingCommands.cs
+++ fCraft/Commands/BuildingCommands.cs
@@ -102,6 +102,7 @@ namespace fCraft {
             //CommandManager.RegisterCommand(CdMessageBlock);
             //CommandManager.RegisterCommand(CdBanX2);
             CommandManager.RegisterCommand(CdDoubleStair);
+            CommandManager.RegisterCommand(CdAbortAll);
         }
         #region LegendCraft
         /* Copyright (c) <2012> <LeChosenOne, DingusBungus, Eeyle>
@@ -226,6 +227,30 @@ THE SOFTWARE.*/
             }
         }
         
+        
+        static readonly CommandDescriptor CdAbortAll = new CommandDescriptor 
+    	{
+			Name = "AbortAll",
+			Aliases = new[] { "abort", "aba" },
+			Category = CommandCategory.Building,
+			Help = "Sets all toggles to false (paint, static, etc)",
+			Handler = AbortAllHandler
+		};
+
+		static void AbortAllHandler( Player player, Command cmd ) 
+		{
+			player.IsPainting = false;				// /paint
+			player.ResetAllBinds();					// /bind
+			player.ParseMessage( "/brush normal", false ); // /brush (totally not a sneaky way to do this)
+			player.BuildingPortal = false;			// /portal
+			player.fireworkMode = false;			// /fireworks
+			player.GunMode = false;					// /gun
+			player.IsRepeatingSelection = false;	// /static
+			player.SelectionCancel();				// /cancel
+			player.StopSpectating();				// /spectate
+			player.towerMode = false;				// /tower
+			player.ParseMessage( "/nvm", false );	// /nvm
+		}
         
         #endregion
 
